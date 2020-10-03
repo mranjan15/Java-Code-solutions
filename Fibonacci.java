5d51979e4e600b042825b24c535028676c1da306
vhegde1 <vaishakh_hegde@intuit.com>
2020-10-03 12:07:33 +0530

    Adding sample java programs

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+
+public class Fibonacci {
+    public static void main(String[] args) {
+    	
+    	//Prints first 12 fibonacci series numbers
+        int n = 12, t1 = 0, t2 = 1;
+        System.out.print("First " + n + " terms: ");
+
+        for (int i = 1; i <= n; ++i)
+        {
+            System.out.print(t1 + " + ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
